4653490855d1bb1fc5475aa4d00a30df6a0935aa
victorG <peter200970@hotmail.com>
2020-12-09 09:10:08 -0500

    Add project-add feature

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,9 +1,55 @@
+import todo from "./todo.js";
+
 const projectsArr = [];
+
+const todolist = [];
 
 const addNewProjectBtn = document.querySelector('.new-project');
 const addNewProjectInput = document.querySelector('.new-project-input');
 let projectsContainer = document.querySelector('.projects');
+const mainContainer = document.querySelector('.main-container');
 
+
+const generateProjectBoard = (project, board) => {
+    while(board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
+   
+    const header = document.createElement('h2');
+    header.textContent = project.textContent;
+    board.appendChild(header);
+
+    let todos = todolist.filter(todo => todo.project == project.textContent);
+
+    if(todos) {
+        todos.forEach(todo => {
+            const title = document.createElement('p');
+            title.textContent = todo.title;
+            board.appendChild(title);
+        });
+    }
+}
+
+
+const justAdded = (text) => {
+    const list = document.querySelector('.list');
+    const project = document.createElement('li');
+    project.classList.add('project-item');
+    project.textContent = text; 
+    
+    project.addEventListener('click', () => {
+        const board = document.querySelector('.display-board');
+        
+        board.classList.add('active-board');
+        
+        generateProjectBoard(project, board);
+    });
+    list.appendChild(project);
+}
+
+const display = () => {
+    console.log(projectsArr);
+};
 
 addNewProjectBtn.addEventListener('click', () => {
     addNewProjectBtn.classList.toggle('active-btn');
@@ -15,12 +61,18 @@ addNewProjectBtn.addEventListener('click', () => {
     }
     if(addNewProjectInput.value) {
         projectsArr.push(addNewProjectInput.value);
+        justAdded(addNewProjectInput.value);
         const form = projectsContainer.querySelector('form');
         form.reset();
     }
     display();
 });
 
-const display = () => {
-    console.log(projectsArr);
-};
+const projectItem = document.querySelectorAll('.project-item');
+
+const newtodo = todo('exercise', 'joggin session', 'tomorrow', 1, 'none', false, 'today');
+
+todolist.push(newtodo);
+
+console.log(todolist);
+
---
+++ src/assets/scripts/todo.js
@@ -0,0 +1,5 @@
+const todo = (title, description, dueDate, priority, notes, checklist, project) => {
+    return {title, description, dueDate, priority, notes, checklist, project};
+}
+
+export default todo;
