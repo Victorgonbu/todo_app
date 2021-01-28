2bca594695918b7a338b2a74ef84d475570521b0
victorG <peter200970@hotmail.com>
2021-01-28 16:32:43 -0500

    Add more tests for Project clas

--- src/assets/tests/project.test.js
+++ src/assets/tests/project.test.js
@@ -2,23 +2,41 @@ import Project from '../scripts/project';
 import Todo from '../scripts/todo';
 
 describe('Project', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
   describe('allOrSpecific', () => {
     it('Return specific-project todos', () => {
-      const projectListArr = () => {
-        const todo = Todo('name', 'description', 'dueDate', 'priority', 'notes', false, 'specific');
-        return [todo];
-      };
-      const project = Project('name', projectListArr);
-      expect(project.allOrSpecific('specific')).toHaveLength(1);
+     
+        const todo = new Todo('name', 'description', 'dueDate', 'priority', 'notes', false, 'specific');
+        todo.saveTodo();
+        expect(Project.allOrSpecific('specific')).toHaveLength(1);
     });
     it('Return All todos', () => {
-      const projectListArr = () => {
-        const todo = Todo('name', 'description', 'dueDate', 'priority', 'notes', false, 'project1');
-        const todo2 = Todo('name', 'description', 'dueDate', 'priority', 'notes', false, 'project2');
-        return [todo, todo2];
-      };
-      const project = Project('name', projectListArr);
-      expect(project.allOrSpecific("All To-Do's")).toHaveLength(2);
+    
+      const todo = new Todo('name', 'description', 'dueDate', 'priority', 'notes', false, 'project1');
+      const todo2 = new Todo('name', 'description', 'dueDate', 'priority', 'notes', false, 'project2');
+      todo.saveTodo();
+      todo2.saveTodo();
+      expect(Project.allOrSpecific("All To-Do's")).toHaveLength(2);
+    });
+  });
+
+  describe('getProjectArr', () => {
+    it('get project array from localStorage', () => {
+      expect(Project.getProjectArr()).toHaveLength(0);
+      const project = new Project('name');
+      project.saveProject();
+      expect(Project.getProjectArr()).toHaveLength(1);
+    });
+  });
+
+  describe('savedProjects', () => {
+    it('get all saved projects from localStorage and display them', () => {
+        const project = new Project('name');
+        project.saveProject();
+        
+        expect(JSON.parse(localStorage.__STORE__['projectarr'])).toHaveLength(1);
     });
   });
 });
--- src/assets/tests/todo.test.js
+++ src/assets/tests/todo.test.js
@@ -3,11 +3,11 @@ import Todo from '../scripts/todo';
 describe('Todo', () => {
   describe('getTodoContainerClass', () => {
     it("get '.undone' class container if todo is not done yet", () => {
-      const todo = Todo('title', 'description', '25-11-2021', 'medium', 'notes', false, 'any');
+      const todo = new Todo('title', 'description', '25-11-2021', 'medium', 'notes', false, 'any');
       expect(todo.getTodoContainerClass(todo.checklist)).toBe('.undone');
     });
     it("get '.done' class container if todo is mark as done", () => {
-      const todo = Todo('title', 'description', '25-11-2021', 'medium', 'notes', true, 'any');
+      const todo = new Todo('title', 'description', '25-11-2021', 'medium', 'notes', true, 'any');
       expect(todo.getTodoContainerClass(todo.checklist)).toBe('.done');
     });
   });
